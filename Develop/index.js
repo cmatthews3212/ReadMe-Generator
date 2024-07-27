03e14cf07b568be8848422089ea4ffbae3cf2782
Christine Matthews <candreola@gmail.com>
2024-07-27 16:07:16 -0500

    create array list of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your application?', 
+    'What is the description of your application?',
+    'How do you install your application?',
+    'How should userse use your application?',
+    'What are any licenses used to create your application?',
+    'Who are those that contributed to the development of your application?',
+    'What are the test instructions for your application?',
+    'What is your GitHub Username?',
+    'What is your email?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
